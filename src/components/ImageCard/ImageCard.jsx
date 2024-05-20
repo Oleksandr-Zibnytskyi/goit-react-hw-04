d5c3eb3bc71a3f407ef327029ce69a0ab1e49bf4
Oleksandr-Zibnytskyi <xander.en.87@gmail.com>
2024-05-20 17:10:40 +0200

    now

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -2,13 +2,14 @@ import css from "./ImageCard.module.css";
 
 export default function ImageCard({ alt, urlSm, urlReg, openModal }) {
   return (
-    <div onClick={() => openModal(alt, urlReg)} className={css.imageCard}>
+    <div className={css.imageCard}>
       <img
         src={urlSm}
         alt={alt}
         width="300"
         height="200"
         className={css.image}
+        onClick={() => openModal(alt, urlReg)}
       />
     </div>
   );
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,22 +1,23 @@
-import css from './ImageGallery.module.css';
-import ImageCard from '../ImageCard/ImageCard';
+import css from "./ImageGallery.module.css";
+import ImageCard from "../ImageCard/ImageCard";
 
-
-export default function ImageGallery ({items, openModal}) {
-
-    return (
-        <>
-           <ul className={css.galleryList}>
-            {items.map((item) => {
-               return (
-                    <li key={item.id} className={css.galleryItem}>
-                        <ImageCard alt={item.alt_description}
-                        urlSm={item.urls.small} urlReg={item.urls.regular} 
-                        likes={item.likes} username={item.user.name} openModal={openModal}/>
-                    </li>
-                )
-            })}
-           </ul>
-        </>
-    )
+export default function ImageGallery({ items, openModal }) {
+  return (
+    <>
+      <ul className={css.galleryList}>
+        {items.map((item) => {
+          return (
+            <li key={item.id} className={css.galleryItem}>
+              <ImageCard
+                alt={item.alt_description}
+                urlSm={item.urls.small}
+                urlReg={item.urls.regular}
+                openModal={openModal}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
